5516eccffe6c6e0ba90b48ad467bc4efe128c260
Máté Cserép <mcserep@gmail.com>
2022-01-21 21:03:03 +0100

    gyro_accelerometer_magnetometer: use Kalman filter accelerometer and gyroscope data

--- gyro_accelerometer_magnetometer/gyro_accelerometer_magnetometer.c
+++ gyro_accelerometer_magnetometer/gyro_accelerometer_magnetometer.c
@@ -2,7 +2,9 @@
 This program reads the angles from the accelerometer and gyroscope
 and the magnetometer data on a BerryIMU connected to a Raspberry Pi.
 
-Based on gyro_accelerometer_tutorial01 and compass_tutorial_01.
+A Kalman filter is used on the accelerometer and gyroscope data.
+
+Based on gyro_accelerometer_tutorial03_kalman_filter and compass_tutorial_01.
 */
 
 #include <sys/time.h>
@@ -13,6 +15,7 @@ Based on gyro_accelerometer_tutorial01 and compass_tutorial_01.
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
 #include <time.h>
 #include "IMU.c"
 
@@ -25,6 +28,20 @@ Based on gyro_accelerometer_tutorial01 and compass_tutorial_01.
 #define RAD_TO_DEG 57.29578
 #define M_PI 3.14159265358979323846
 
+
+//Used by Kalman Filters
+float Q_angle  =  0.01;
+float Q_gyro   =  0.0003;
+float R_angle  =  0.01;
+float x_bias = 0;
+float y_bias = 0;
+float XP_00 = 0, XP_01 = 0, XP_10 = 0, XP_11 = 0;
+float YP_00 = 0, YP_01 = 0, YP_10 = 0, YP_11 = 0;
+float KFangleX = 0.0;
+float KFangleY = 0.0;
+
+float kalmanFilterX(float accAngle, float gyroRate);
+float kalmanFilterY(float accAngle, float gyroRate);
 
 void  INThandler(int sig)// Used to do a nice clean exit when Ctrl-C is pressed
 {
@@ -76,7 +93,7 @@ int main(int argc, char *argv[])
   gettimeofday(&tvBegin, NULL);
 
   //printf("Loop Time 0\t");
-  printf("LoopTime,GyroX,AccXangle,CFangleX,GyroY,AccYangle,CFangleY,MagRawX,MagRawY,MagRawZ,Heading");
+  printf("LoopTime,kalmanX,kalmanY,GyroX,AccXangle,CFangleX,GyroY,AccYangle,CFangleY,MagRawX,MagRawY,MagRawZ,Heading");
   printf("0,");
   while(1)
   {
@@ -119,6 +136,11 @@ int main(int argc, char *argv[])
     else
       AccYangle += (float)90;
 
+    //Kalman Filter
+    float kalmanX = kalmanFilterX(AccXangle, rate_gyr_x);
+    float kalmanY = kalmanFilterY(AccYangle, rate_gyr_y);
+    //printf ("\033[22;31mkalmanX %7.3f  \033[22;36mkalmanY %7.3f\t\e[m",kalmanX,kalmanY);
+    printf("%.3f,%.3f,",kalmanX,kalmanY);
 
     //Complementary filter used to combine the accelerometer and gyro values.
     CFangleX=AA*(CFangleX+rate_gyr_x*DT) +(1 - AA) * AccXangle;
@@ -149,4 +171,57 @@ int main(int argc, char *argv[])
     //printf("Loop Time %d\t", mymillis()- startInt);
     printf("%d,", mymillis()- startInt);
   }
+}
+
+
+float kalmanFilterX(float accAngle, float gyroRate){
+	float  y, S;
+	float K_0, K_1;
+
+	KFangleX += DT * (gyroRate - x_bias);
+
+	XP_00 +=  - DT * (XP_10 + XP_01) + Q_angle * DT;
+	XP_01 +=  - DT * XP_11;
+	XP_10 +=  - DT * XP_11;
+	XP_11 +=  + Q_gyro * DT;
+
+	y = accAngle - KFangleX;
+	S = XP_00 + R_angle;
+	K_0 = XP_00 / S;
+	K_1 = XP_10 / S;
+
+	KFangleX +=  K_0 * y;
+	x_bias  +=  K_1 * y;
+	XP_00 -= K_0 * XP_00;
+	XP_01 -= K_0 * XP_01;
+	XP_10 -= K_1 * XP_00;
+	XP_11 -= K_1 * XP_01;
+
+	return KFangleX;
+}
+
+float kalmanFilterY(float accAngle, float gyroRate){
+	float  y, S;
+	float K_0, K_1;
+
+	KFangleY += DT * (gyroRate - y_bias);
+
+	YP_00 +=  - DT * (YP_10 + YP_01) + Q_angle * DT;
+	YP_01 +=  - DT * YP_11;
+	YP_10 +=  - DT * YP_11;
+	YP_11 +=  + Q_gyro * DT;
+
+	y = accAngle - KFangleY;
+	S = YP_00 + R_angle;
+	K_0 = YP_00 / S;
+	K_1 = YP_10 / S;
+
+	KFangleY +=  K_0 * y;
+	y_bias  +=  K_1 * y;
+	YP_00 -= K_0 * YP_00;
+	YP_01 -= K_0 * YP_01;
+	YP_10 -= K_1 * YP_00;
+	YP_11 -= K_1 * YP_01;
+
+	return KFangleY;
 }
